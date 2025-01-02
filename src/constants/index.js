a290182eec8abf11330e95160a120fe3ad060784
Maxim Ignatev <ignatif@gmail.com>
2025-01-02 18:41:20 +0400

    init theme

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+const BLUE = '96C8FF'
+const LIME = 'DCFF1E'
+const LILIAC = 'E6D2FF'
+const ORANGE = 'FF6E00'
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        host: ['var(--font-host-grotesk)'],
+        space: ['var(--font-space-grotesk)'],
+        climate: ['var(--font-climate-crisis)'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: '#F6F5FA',
+        card: '#ECEBF1',
+        cardDark: '#222224',
+
+        blue: '#96C8FF',
+        lime: '#DCFF1E',
+        liliac: '#E6D2FF',
+        orange: '#FF6E00',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
+
+// border-radius: 32px;
+// border: 1px solid rgba(255, 255, 255, 0.60);
+// background: rgba(255, 255, 255, 0.20);
+// backdrop-filter: blur(75px);
