ee21aeabc262d1c94d057bc3328becb988d9cef0
Maxim Ignatev <ignatif@gmail.com>
2025-01-14 16:16:33 +0400

    ray animation v1

--- src/app/animations/ray/page.tsx
+++ src/app/animations/ray/page.tsx
@@ -1,46 +1,85 @@
+'use client'
 import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 interface RayProps {
   angle: number
-  isOuterRay: boolean
+  isOdd: boolean
 }
 
-const Ray: React.FC<RayProps> = ({ angle, isOdd }) => (
-  <div
-    className="absolute"
-    style={{
-      height: '2px',
-      width: '100px',
-      // background: 'rgb(240, 240, 245)',
-      background: isOdd ? 'red' : 'blue',
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: `
-        rotate(${angle}deg)
-        translate(${isOdd ? '100px' : '100px'}, -50%)
-      `,
-    }}
-  />
-)
+const Ray: React.FC<RayProps> = ({ angle, isOdd }) => {
+  const [isSwapped, setIsSwapped] = React.useState(false)
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setIsSwapped((prev) => !prev)
+    }, 2000) // Swap every 2 seconds
+
+    return () => clearInterval(interval)
+  }, [])
+
+  const currentScale = isSwapped
+    ? isOdd
+      ? 1.1
+      : 0.9 // Swapped state
+    : isOdd
+      ? 0.9
+      : 1.1 // Initial state
+
+  const currentOrigin = isSwapped
+    ? isOdd
+      ? '0% 50%'
+      : '100% 50%' // Swapped state
+    : isOdd
+      ? '100% 50%'
+      : '0% 50%' // Initial state
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: `rotate(${angle}deg) translate(260px, -50%)`,
+      }}
+    >
+      <motion.div
+        style={{
+          height: '2px',
+          width: '188px',
+          background: isOdd ? 'red' : 'blue',
+          // background: 'rgb(240, 240, 245)',
+        }}
+        animate={{
+          scaleX: currentScale,
+          transformOrigin: currentOrigin,
+        }}
+        transition={{
+          duration: 2,
+          ease: 'easeInOut',
+        }}
+      />
+    </div>
+  )
+}
 
 const RayBackground: React.FC = () => {
   const angles: number[] = Array.from({ length: 16 }, (_, i) => i * (360 / 16))
 
   return (
     <div className="relative w-full h-screen overflow-hidden bg-white">
-      <div className="absolute inset-0 ">
+      <div className="absolute inset-0">
         {angles
-          .filter((i, ind) => ind % 2 === 0)
+          .filter((_, ind) => ind % 2 === 0)
           .map((angle) => (
             <Ray key={`primary-${angle}`} angle={angle} isOdd={true} />
           ))}
       </div>
-      <div className="absolute inset-0 ">
+      <div className="absolute inset-0">
         {angles
-          .filter((i, ind) => ind % 2 !== 0)
+          .filter((_, ind) => ind % 2 !== 0)
           .map((angle) => (
-            <Ray key={`primary-${angle}`} angle={angle} isOdd={false} />
+            <Ray key={`secondary-${angle}`} angle={angle} isOdd={false} />
           ))}
       </div>
     </div>
