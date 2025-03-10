d33c551709e89890180d85a7f0e8a79118c8883e
Dmitry <leedmitry8091@gmail.com>
2025-03-10 23:35:22 +1000

    revert about/page.tsx

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -6,44 +6,50 @@ import Footer from '@/components/footer'
 import { useTranslations } from 'next-intl'
 
 const AboutPage = () => {
-  const lyrics = ['l1', 'l2', 'l3', 'l4']
+  const lyrics = [
+    'l1',
+    'l2',
+    'l3',
+    'l4',
+  ]
 
   return (
     <main>
-      <div className="snap-y snap-mandatory h-screen">
-        {/* Первый текстовый блок */}
-        <section
-          key="first-section"
-          className="h-screen w-full flex items-center justify-center snap-start first-snap-section"
-        >
-          <AnimatedText text={lyrics[0]} />
-        </section>
-
-        {/* Остальные текстовые блоки */}
-        {lyrics.slice(1).map((text, index) => (
+      {/*<div className="snap-y snap-mandatory overflow-y-auto h-screen">*/}
+        <div className="snap-y snap-mandatory h-screen">
+          {/* First section needs an id for the observer */}
           <section
-            key={index + 1}
-            className="h-screen w-full flex items-center justify-center snap-start"
+              key="first-section"
+              className="h-screen w-full flex items-center justify-center snap-start first-snap-section"
           >
-            <AnimatedText text={text} />
+            <AnimatedText text={lyrics[0]}/>
           </section>
-        ))}
 
-        {/* Карусель изображений */}
-        <section className="h-max w-full snap-start pb-20 pt-28 lg:pt-22 md:pt-24 2xl:pt-20 no-snap">
-          <ImageCarousel />
-        </section>
+          {/* Rest of the sections */}
+          {lyrics.slice(1).map((text, index) => (
+              <section
+                  key={index + 1}
+                  className="h-screen w-full flex items-center justify-center snap-start"
+              >
+                <AnimatedText text={text}/>
+              </section>
+          ))}
 
-        {/* Футер */}
-        <section className="snap-start">
-          <Footer />
-        </section>
-      </div>
+          <section className="h-max w-full snap-start pb-20 pt-28 lg:pt-22 md:pt-24 2xl:pt-20">
+            <ImageCarousel/>
+          </section>
+
+          <section className="snap-start">
+            <Footer/>
+          </section>
+        </div>
     </main>
-  )
+)
 }
 
-const AnimatedText = ({ text }: { text: string }) => {
+const AnimatedText = ({text}: {
+  text: string
+}) => {
   const ref = React.useRef(null)
   const isInView = useInView(ref, {
     once: false,
@@ -68,7 +74,7 @@ const AnimatedText = ({ text }: { text: string }) => {
         mass: 1,
       }}
     >
-      <div className="gradient-text text-[56px] leading-[79.68px] md:text-[96px] font-host font-extrabold">
+      <div className="gradient-text text-[56px] leading-[79.68px] md:text-[96px] font-medium font-host font-extrabold">
         {t(text)}
       </div>
     </motion.div>
